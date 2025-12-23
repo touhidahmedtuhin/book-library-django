22151c45f922a49e201798a994c3a9b81643839e
touhidahmedtuhin <tuhintouhidahmed@gmail.com>
2025-12-23 09:48:54 +0600

    add the filter option

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -4,6 +4,7 @@ from .models import Book
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
   prepopulated_fields = {"slug": ("title",)}
+  list_filter = ("author","rating",)
 
 
 admin.site.register(Book,BookAdmin)
