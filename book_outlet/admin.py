f1b4f8dd0aa6b7991d1b560aa1849499d621510f
touhidahmedtuhin <tuhintouhidahmed@gmail.com>
2025-12-25 09:13:17 +0600

    change in the models

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,11 +1,32 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book , Author , Address
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
   prepopulated_fields = {"slug": ("title",)}
   list_filter = ("author","rating",)
-  list_display = ("title","author")
+  list_display = ("title","author_name")
+
+  def author_name(self, obj):
+        return f"{obj.author.first_name} {obj.author.last_name}"
+
 
 
 admin.site.register(Book,BookAdmin)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("full_name",)
+    search_fields = ("first_name", "full_name",)
+
+    def full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ("street","postal_code","city",)
+
+
+
+
+
